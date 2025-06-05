0182bbd1deabd8cd305b07b123a629974802b49a
Jesus Soto <jhsotoglz@gmail.com>
2025-06-05 16:14:59 -0500

    Setting up content components for future development

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,7 +1,9 @@
 const About = () => {
     return (
         <section id="about" className="h-screen flex justify-center items-center bg-gray-100">
-            <h1 className="text-4xl font-bold">About Me</h1>
+            <div className="global-content">
+                <h1 className="text-4xl font-bold">About Me</h1>
+            </div>
         </section>
     );
 };
--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,7 +1,9 @@
 const Projects = () => {
     return (
         <section id="projects" className="h-screen flex justify-center items-center bg-white">
-            <h1 className="text-4xl font-bold">My Projects</h1>
+            <div className="global-content">
+                <h1 className="text-4xl font-bold">My Projects</h1>
+            </div>
         </section>
     );
 };
