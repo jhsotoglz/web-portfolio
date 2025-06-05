16c204301939062ae7dd0a3df9386e79d5ea1ca8
Jesus Soto <jhsotoglz@gmail.com>
2025-06-05 16:12:08 -0500

    Fixed content screen size and cleaned.

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,6 @@ const Home = () => {
     <section id="home" className="h-screen flex justify-center items-center bg-white">
       <div className="global-content">
         <h1 className="text-4xl font-bold">Welcome to my portfolio</h1>
-        <h3 className="text-4xl font-bold">--------------------------------------------------------------------------------------------------------------</h3>
       </div>
     </section>
   );
