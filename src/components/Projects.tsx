35b38abb41b5cd326e9c8afb97186576a0e70b44
Jesus Soto <jhsotoglz@gmail.com>
2025-07-07 16:15:55 -0500

    Developing Projects web application component

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -1,6 +1,6 @@
 const Projects = () => {
     return (
-        <section id="projects" className="min-h-screen-dynamic flex justify-center items-center bg-white">
+        <section id="projects" className="min-h-screen-dynamic flex justify-center items-center">
             <div className="global-content">
                 <h1 className="text-4xl font-bold">My Projects</h1>
             </div>
