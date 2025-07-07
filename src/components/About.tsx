3a0889e13d03c1b7b8f86d97997362ea2762d0e9
Jesus Soto <jhsotoglz@gmail.com>
2025-07-07 17:14:39 -0500

    Better looking project component

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -24,10 +24,10 @@ const About = () => {
     return (
         <section
             id="about"
-            className="px-4 scroll-mt-16 text-white flex flex-col items-center lg:-mt-40 mb-8"
+            className="px-4 scroll-mt-16 text-white flex flex-col items-center lg:-mt-40 mb-20"
         >
             <div className="w-full max-w-4xl text-center">
-                <h1 className="text-4xl font-bold mb-6">
+                <h1 className="text-4xl font-bold mb-6 mt-6">
                     About <span className="text-secondary">Me</span>
                 </h1>
 
