fb494551eab01e4f64e2d7e9291450ad7ab5f563
Jesus Soto <jhsotoglz@gmail.com>
2025-07-12 13:36:20 -0500

    Project Data updated for Project 2

--- src/data/projectData.ts
+++ src/data/projectData.ts
@@ -25,5 +25,17 @@ export const projects: Project[] = [
     ],
     images: [GridAIPoster]
   },
-  // Add more projects here...
+  {
+    title: "Probabilistic Escape-Persuit Planning",
+    description: "An all-in-one web app for electric grid monitoring...",
+    tech: ["React", "Kafka", "Firebase", "Tailwind CSS", "TypeScript"],
+    link: undefined,
+    youtube: "https://www.youtube.com/embed/H-c5zdhf0zs",
+    details: `GridAI is a real-time dashboard...`,
+    pdfs: [
+      { name: "Design Doc", url: "/docs/gridai_design.pdf" },
+      { name: "Final Report", url: "/docs/gridai_report.pdf" },
+    ],
+    images: [GridAIPoster]
+  },
 ];
